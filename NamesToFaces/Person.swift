0071f9e2a001150ee8b9d4f8f4dd948d40b3d2aa
giovannapezzini <giovannarpezzini@gmail.com>
2021-02-02 22:14:40 -0300

    Conform to Codable

--- NamesToFaces/Person.swift
+++ NamesToFaces/Person.swift
@@ -7,22 +7,12 @@
 
 import UIKit
 
-class Person: NSObject, NSCoding {
+class Person: NSObject, Codable {
     var name: String
     var image: String
-    
-    required init?(coder: NSCoder) {
-        name = coder.decodeObject(forKey: "name") as? String ?? ""
-        image = coder.decodeObject(forKey: "image") as? String ?? ""
-    }
     
     init(name: String, image: String) {
         self.name = name
         self.image = image
-    }
-    
-    func encode(with coder: NSCoder) {
-        coder.encode(name, forKey: "name")
-        coder.encode(image, forKey: "image")
     }
 }
